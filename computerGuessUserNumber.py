b6021e0e5b7213b26b2863348c496d07fb83ccd2
Mi1Col <143884635+Mi1Col@users.noreply.github.com>
2024-04-29 17:45:46 +0200

    computerGuessUserNumber.py

--- computerGuessUserNumber.py
+++ computerGuessUserNumber.py
@@ -1,18 +1,22 @@
 import random
 
 def computerGuess(x):
+
     low = 1
     high = x
     feedback = ""
     tries = 0
+
     while feedback != "=":
         guess = random.randint(low, high)
-        feedback = input(f"{guess} is too high(+) or too low(-)? ")
-        if feedback == "+":
+        feedback = input(f"{guess} is too high(h) or too low(l)? ")
+
+        if feedback == "h":
             high = guess -1
-        elif feedback == "-":
+        elif feedback == "l":
             low = guess +1
         tries += 1
+        
     if tries == 1:
         print(f"""
     The number is {guess}.
